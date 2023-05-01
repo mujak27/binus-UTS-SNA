803f79574652d1dd0f139c0f78dd5924635b7f92
mujak27 <mujakoverflow@gmail.com>
2023-05-01 00:55:11 +0000

    mysql still refusing connection

--- db/redlock.sql
+++ db/redlock.sql
@@ -1,27 +1,4 @@
--- MySQL dump 10.13  Distrib 8.0.32, for Linux (x86_64)
---
--- Host: localhost    Database: redlock
--- ------------------------------------------------------
--- Server version	8.0.32-0ubuntu0.20.04.2
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `users`
---
-
 DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
   `id` int NOT NULL,
   `name` varchar(100) DEFAULT NULL,
@@ -29,25 +6,8 @@ CREATE TABLE `users` (
   `position` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
 
 LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` VALUES (1,'irkimuzak','jakarta','remote developer');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2023-04-29  7:53:14
+GRANT ALL ON redlock.* TO user@'172.28.5.1' IDENTIFIED BY 'password';
--- php/app/index.php
+++ php/app/index.php
@@ -1,20 +1,36 @@
 <?php
-//These are the defined authentication environment in the db service
 
-// The MySQL service named in the docker-compose.yml.
-$host = 'netmysq';
+$host = 'redlock-db-1';
+$user = 'user';
+$pass = 'password';
+$db = 'redlock';
+$port = 3400;
 
-// Database use name
-$user = 'root';
-
-//database user password
-$pass = '';
-
-// check the MySQL connection status
-$conn = new mysqli($host, $user, $pass, "redlock", "3400");
+$conn = new mysqli($host, $user, $pass, $db, $port);
 if ($conn->connect_error) {
+    echo "failed";
     die("Connection failed: " . $conn->connect_error);
 } else {
     echo "Connected to MySQL server successfully!";
 }
+
+
+$tableName="users";
+$columns= ['id', 'name','address','position'];
+$msg="";
+
+$columnName = implode(", ", $columns);
+$query = "SELECT ".$columnName." FROM $tableName";
+$result = $db->query($query);
+if($result== true){ 
+    if ($result->num_rows > 0) {
+        $row= mysqli_fetch_all($result, MYSQLI_ASSOC);
+        $msg= $row;
+    } else {
+        $msg= "No Data Found"; 
+    }
+}else{
+    $msg= mysqli_error($db);
+}
+echo $msg;
 ?>
